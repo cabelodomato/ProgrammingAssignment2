2fc6d313d9e1b0e23e17aa0dd4aa5dc9fcb9f113
Danilo Castro de Souza <danilo@cabelodomato.com.br>
2014-09-22 01:23:50 +1000

    Assignment 2 done with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair functions, makeCacheMatrix and cacheSolve, are used together to make computations 'on the fly' and store(cache) it results;
+## as well it retrieve saved values from previous computations, ainming to avoid redundance in the computation.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix returns a list of functions (get,set,getmean,setmean) for the value given as argument,
+## while caching the matrix(argument) and its inverse
 makeCacheMatrix <- function(m = matrix()) {
   inverse <- NULL
   set <- function(y) {
@@ -15,8 +15,9 @@ makeCacheMatrix <- function(m = matrix()) {
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-## Write a short comment describing this function
-
+## cacheSolve makes use of the list created by makeCacheMatrix.
+## It first gets the inverse of matrix given at makeCacheMatrix, and display the result,
+## or calculates the inverse matrix given at makeCacheMatrix and stores, given the first request for the inverse matrix were null.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inverse <- x$getinverse()
